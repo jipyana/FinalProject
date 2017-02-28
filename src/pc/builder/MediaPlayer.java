44063f4e4583644eebacd3c90d5bcd345e5d4c4a
Jamila <jipyana@student.neumont.edu>
2017-02-27 19:42:01 -0700

    Final Project

---
+++ src/pc/builder/MediaPlayer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pc.builder;
+
+import java.net.URL;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Jamila
+ */
+public class MediaPlayer extends JFrame{
+    
+    public MediaPlayer(){
+//        URL player = "https://www.youtube.com/watch?v=N4gU8BdloDE';
+    }
+            
+            
+}
---
+++ src/pc/builder/PCBuilder.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pc.builder;
+
+import javax.swing.JFrame;
+
+
+/**
+ *
+ * @author Jamila
+ */
+public class PCBuilder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame start = new MainFrame();
+        start.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        start.setVisible(true);
+
+//        BuildYourPCWindow pcBuilder = new BuildYourPCWindow();
+//        pcBuilder.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        pcBuilder.setVisible(true);
+    }
+    
+}
