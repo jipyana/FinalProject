6ce8d39ef34ef5ae9720f3e1a512efd0b5cfa223
Jamila <jipyana@student.neumont.edu>
2017-03-08 16:18:08 -0700

    Motherboard and Operating Systems
    Updated text files for operating systems and motherboards.

--- src/pc/builder/PCBuilder.java
+++ src/pc/builder/PCBuilder.java
@@ -22,9 +22,15 @@ import javax.swing.JInternalFrame;
  *
  * @author Jamila
  */
+
+
+/*
+ * Add the path to the parameters instead of having different methods for each individual card
+ */
 public class PCBuilder {
     
     public static final ArrayList<SpecificationsForVideoCards> videoCards = new ArrayList();
+    public static final ArrayList<SpecificationsForOS> OS = new ArrayList();
     private static File geforceGTX460 = new File("C:\\Users\\Jamila\\Documents\\NetBeansProjects\\PC Builder\\FinalProject\\src\\PCComponents\\Video Cards\\GeForce GTX 460");
     private static File geforceGTX1050TI = new File("C:\\Users\\Jamila\\Documents\\NetBeansProjects\\PC Builder\\FinalProject\\src\\PCComponents\\Video Cards\\GeForce GTX 1050Ti");
     private static File geforceGTX1060 = new File("C:\\Users\\Jamila\\Documents\\NetBeansProjects\\PC Builder\\FinalProject\\src\\PCComponents\\Video Cards\\GeForce GTX 1060");
@@ -35,6 +41,17 @@ public class PCBuilder {
     private static File geforceGTXTitanZ = new File("C:\\Users\\Jamila\\Documents\\NetBeansProjects\\PC Builder\\FinalProject\\src\\PCComponents\\Video Cards\\GeForce GTX Titan Z");
     private static File geforceGTXTitanX =  new File("C:\\Users\\Jamila\\Documents\\NetBeansProjects\\PC Builder\\FinalProject\\src\\PCComponents\\Video Cards\\GeForce GTX Titan X");
     
+    private static File windows7Home_32_64Bit = new File("C:\\Users\\Jamila\\Documents\\NetBeansProjects\\PC Builder\\FinalProject\\src\\PCComponents\\Operating Systems\\Microsoft Windows 7 Home Premium (32/64-bit");
+    private static File windows7Home_32Bit = new File("C:\\Users\\Jamila\\Documents\\NetBeansProjects\\PC Builder\\FinalProject\\src\\PCComponents\\Operating Systems\\Microsoft Windows 7 Home Premium SP1 (32-bit)");
+    private static File windows7Home_64Bit = new File("C:\\Users\\Jamila\\Documents\\NetBeansProjects\\PC Builder\\FinalProject\\src\\PCComponents\\Operating Systems\\Microsoft Windows 7 Home Premium SP1 (64-bit)");
+    private static File windows8_32Bit = new File("C:\\Users\\Jamila\\Documents\\NetBeansProjects\\PC Builder\\FinalProject\\src\\PCComponents\\Operating Systems\\Microsoft Windows 8 (32-bit)");
+    private static File windows8_64Bit = new File("C:\\Users\\Jamila\\Documents\\NetBeansProjects\\PC Builder\\FinalProject\\src\\PCComponents\\Operating Systems\\Microsoft Windows 8 (64-bit)");
+    private static File windows8_1_32_64Bit = new File("C:\\Users\\Jamila\\Documents\\NetBeansProjects\\PC Builder\\FinalProject\\src\\PCComponents\\Operating Systems\\Microsoft Windows 8.1 (32-64-bit)");
+    private static File windows8_1_32Bit = new File("C:\\Users\\Jamila\\Documents\\NetBeansProjects\\PC Builder\\FinalProject\\src\\PCComponents\\Operating Systems\\Microsoft Windows 8.1 (32-bit)");
+    private static File windows8_1_64Bit = new File("C:\\Users\\Jamila\\Documents\\NetBeansProjects\\PC Builder\\FinalProject\\src\\PCComponents\\Operating Systems\\Microsoft Windows 8.1 (64-bit)");
+    private static File windows10Home_32_64Bit = new File("C:\\Users\\Jamila\\Documents\\NetBeansProjects\\PC Builder\\FinalProject\\src\\PCComponents\\Operating Systems\\Microsoft Windows 10 Home (32-64-bit)");
+    private static File windows10Home_32Bit = new File("C:\\Users\\Jamila\\Documents\\NetBeansProjects\\PC Builder\\FinalProject\\src\\PCComponents\\Operating Systems\\Microsoft Windows 10 Home (32-bit)");
+    private static File windows10Home_64Bit = new File("C:\\Users\\Jamila\\Documents\\NetBeansProjects\\PC Builder\\FinalProject\\src\\PCComponents\\Operating Systems\\Microsoft Windows 10 Home (64-bit)");
  
     /**
      * @param args the command line arguments
@@ -55,7 +72,20 @@ public class PCBuilder {
             fileReaderGTX1080(geforceGTX1080);
             fileReaderGTXTitanZ(geforceGTXTitanZ);
             fileReaderGTXTitanX(geforceGTXTitanX);
-       
+            
+            
+            fileReaderoSperatingSystem(windows7Home_32_64Bit);
+            fileReaderoSperatingSystem(windows7Home_32Bit);
+            fileReaderoSperatingSystem(windows7Home_64Bit);
+            fileReaderoSperatingSystem(windows8_32Bit);
+            fileReaderoSperatingSystem(windows8_64Bit);
+            fileReaderoSperatingSystem(windows8_1_32_64Bit);
+            fileReaderoSperatingSystem(windows8_1_32Bit);
+            fileReaderoSperatingSystem(windows8_1_64Bit);
+            fileReaderoSperatingSystem(windows10Home_32_64Bit);
+            fileReaderoSperatingSystem(windows10Home_32Bit);
+            fileReaderoSperatingSystem(windows10Home_64Bit);
+            
     }
     
     public static void fileReaderGTX460(File geforceGTX460){
@@ -445,4 +475,34 @@ public class PCBuilder {
        }
         
    }    
+    
+//    question about this: Ask Marquis
+    public static void fileReaderoSperatingSystem(File oSperatingSystem){
+    	
+    	for(File item : oSperatingSystem.listFiles()){
+    		
+    		try(BufferedReader br = new BufferedReader(new FileReader(item))){
+    			String line = null;
+
+    			SpecificationsForOS oS = new SpecificationsForOS();
+    			int lineNumber = 0;
+                while ((line = br.readLine()) != null) {
+                    lineNumber++;
+                    if(lineNumber == 1){
+                    	oS.setoSName(line.trim());
+                    } else if(lineNumber == 3){
+                    	oS.setPrice((Double.parseDouble(line.trim().replaceAll("Price:", "").replaceAll("\\$", "").trim())));
+                    }
+                    OS.add(oS);
+                }
+    		} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    		
+    	}
+    }
 }
---
+++ src/pc/builder/SpecificationsForOS.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pc.builder;
+
+/**
+ *
+ * @author Jamila
+ */
+
+public class SpecificationsForOS {
+	
+	private String oSName;
+	private double price;
+	
+	public SpecificationsForOS(){
+		
+	}
+
+	public String getoSName() {
+		return oSName;
+	}
+
+	public void setoSName(String oSName) {
+		this.oSName = oSName;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
+
