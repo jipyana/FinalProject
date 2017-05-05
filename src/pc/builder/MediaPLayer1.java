80d0fbe021e9608827bf3149a3181abc16b7135e
Jamila <jipyana@student.neumont.edu>
2017-05-05 08:50:04 -0600

    Last Commits

--- src/pc/builder/MediaPLayer1.java
+++ src/pc/builder/MediaPLayer1.java
@@ -33,11 +33,11 @@ public class MediaPLayer1 extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1143, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 819, Short.MAX_VALUE)
         );
 
         pack();
